06c00d13d8bb024912660df0562ff1ac08fbe238
Hirotaka Kawashima <k.hirotaka@me.com>
2021-04-21 22:41:58 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+import os
+import sys
+from glob import glob
 from setuptools import setup
+
+DIR = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.join(DIR, "third_party", "pybind11"))
+
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 __version__ = "0.0.1"
 ext_modules = [
     Pybind11Extension(
         "pycpp",
-        ["src/main.cpp"],
+        sorted(glob("src/*.cpp")),
         define_macros=[("VERSION_INFO", __version__)],
     ),
 ]
