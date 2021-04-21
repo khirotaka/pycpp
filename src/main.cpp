8bc2c3940328e483366f7492937f19db4c774ef5
Hirotaka Kawashima <k.hirotaka@me.com>
2021-04-21 22:41:44 +0900

    create multiply_array function

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,32 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+#include <Eigen/Dense>
+
+
+/*
+ ndarrayはメモリ配置がデフォルトではC言語で主に用いられるRowMajorなのに対し、
+ EigenではデフォルトはfortranのColumnMajorである。
+ ただ、両者とも指定することができるのでここではndarrayのRowMajorに揃える。
+ 毎回RowMajorを宣言するのも面倒なのでRMatrixというテンプレートクラスを作っておく。
+ 
+ Reference: https://qiita.com/lucidfrontier45/items/5048ef74fbf32eeb9f08
+ */
+template <typename T>
+using RMatrix = Eigen::Matrix<T, -1, -1, Eigen::RowMajor>;
+
 
 int add(int x, int y) {
     return x + y;
+}
+
+template <typename T>
+RMatrix<T> multiply_array(RMatrix<T> m, T value) {
+    return m * value;
 }
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(pycpp, m) {
     m.def("add", &add, "Add two numbers");
+    m.def("multiply_array", &multiply_array<double>, "");
 }
