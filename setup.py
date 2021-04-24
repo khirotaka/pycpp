07115ef7ab8d3853f79b9afb3413cb81c62d888f
Hirotaka Kawashima <k.hirotaka@me.com>
2021-04-24 21:01:29 +0900

    PythonのコードとC++のコードを共存できるように改良

--- setup.py
+++ setup.py
@@ -1,25 +1,74 @@
+import io
 import os
+import re
 import sys
-from glob import glob
-from setuptools import setup
+import platform
+import subprocess
 
-DIR = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(os.path.join(DIR, "third_party", "pybind11"))
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
 
-from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-__version__ = "0.0.1"
-ext_modules = [
-    Pybind11Extension(
-        "pycpp",
-        sorted(glob("src/*.cpp")),
-        define_macros=[("VERSION_INFO", __version__)],
-    ),
-]
+# https://github.com/pybind/cmake_example/blob/c45488dfdff04eec43fd2e59fcf9d9cd21b83880/setup.py
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
+
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
 
 setup(
     name="pycpp",
     version="0.0.1",
-    ext_modules=ext_modules,
-    cmdclass={"build_ext": build_ext},
+    ext_modules=[CMakeExtension("pycpp.functions._C")],
+    cmdclass={"build_ext": CMakeBuild},
 )
