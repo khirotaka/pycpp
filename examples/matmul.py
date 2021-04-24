dc45385e19ef630d864aefa6b27ab0c656bd6b0c
Hirotaka Kawashima <k.hirotaka@me.com>
2021-04-24 21:03:53 +0900

    matmulを利用した例を作成

---
+++ examples/matmul.py
@@ -0,0 +1,39 @@
+import time
+import numpy as np
+from pycpp.functions._C import matmul
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        print("{:.4f} s".format(end - start))
+    return wrapper
+
+
+@timer
+def np_matmul(array: np.ndarray):
+    np.matmul(array, array.T)
+
+
+@timer
+def blas_matmul(array: np.ndarray):
+    matmul(array, array.T)
+
+
+def main():
+    np.random.seed(0)
+    array = np.random.randn(5000, 5000).astype(np.float32)
+
+    print("BLAS(AMX) Speed: ")
+    blas_matmul(array)
+
+    print("=" * 50)
+    print("NumPy Speed: ")
+    np_matmul(array)
+    print("=" * 50)
+
+
+if __name__ == '__main__':
+    main()
