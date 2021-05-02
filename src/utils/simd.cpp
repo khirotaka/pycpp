7dd76bae7abee340ec6f5f7b6bdd9042c86e22e2
Hirotaka Kawashima <k.hirotaka@me.com>
2021-05-02 21:27:58 +0900

    create print_hello function

--- src/utils/simd.cpp
+++ src/utils/simd.cpp
@@ -1,6 +1,14 @@
+#include <iostream>
 #include <pybind11/pybind11.h>
 #include <Eigen/Eigen>
+#include <omp.h>
 
+void print_hello() {
+    #pragma omp parallel
+    {
+        std::cout << "Hello, World" << std::endl;
+    }
+}
 
 std::string available_simd_instructions() {
     return Eigen::SimdInstructionSetsInUse();
@@ -11,4 +19,5 @@ namespace py = pybind11;
 PYBIND11_MODULE(utils, m) {
     m.def("available_simd_instructions", &available_simd_instructions,
           "Returns a string indicating the available SIMD instructions");
+    m.def("print_hello", &print_hello, "");
 }
